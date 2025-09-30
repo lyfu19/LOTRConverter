d20b7eea1d8b0c5480f75dc0b44b7d49111305a3
Yongfu Lin <alfielin0709@gmail.com>
2025-09-30 23:46:01 +1300

    Initial Commit

---
+++ LOTRConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LOTRConverter
+//
+//  Created by 阿福 on 30/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by 阿福 on 30/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
